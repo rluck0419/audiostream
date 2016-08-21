9865b06b76202b4c417271dbfc862c63d64bf453
Robert Luckfield <rluck0419@gmail.com>
2016-08-21 00:20:29 -0500

    four instruments in database - modified scale page to always start with root note (key) in lowest octave

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -9,23 +9,41 @@ class ScalesController < ApplicationController
     # chord2 = Chord.third
     key = Key.all.sample
     reverb = Reverb.first
-    piano_notes = []
-    harp_notes = []
-    marimba_notes = []
+    # piano_notes = []
+    # harp_notes = []
+    # marimba_notes = []
+    # squarewave_notes = []
     all_notes = Note.all
     notes = notes_in_key_and_scale(key.name, scale)
+    final_notes = []
 
     all_notes.each_with_index do |note, index|
       if notes.include?(note.name) && note.instrument.name == "piano"
-        piano_notes << note
+        final_notes << note
+        # piano_notes << note
       end
       if notes.include?(note.name) && note.instrument.name == "harp"
-        harp_notes << note
+        final_notes << note
+        # harp_notes << note
       end
       if notes.include?(note.name) && note.instrument.name == "marimba"
-        marimba_notes << note
+        final_notes << note
+        # marimba_notes << note
+      end
+      if notes.include?(note.name) && note.instrument.name == "squarewave"
+        final_notes << note
+        # squarewave_notes << note
       end
     end
-    render locals: { piano_notes: piano_notes, harp_notes: harp_notes, marimba_notes: marimba_notes, reverb: reverb }
+    final_notes = final_notes.shuffle
+
+    popped_note = final_notes.first
+    until popped_note.name == key.name && popped_note.octave == 2
+      popped_note = final_notes.pop
+      final_notes = [popped_note, final_notes].flatten
+    end
+
+    render locals: { notes: final_notes, reverb: reverb, key: key.name}
+    # render locals: { piano_notes: piano_notes, harp_notes: harp_notes, marimba_notes: marimba_notes, squarewave_notes: squarewave_notes, reverb: reverb }
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,10 @@
 NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
 piano = Instrument.create!(name: "piano")
-harp = Instrument.create!(name: "harp")
-marimba = Instrument.create!(name: "marimba")
+Instrument.create!(name: "harp")
+Instrument.create!(name: "marimba")
+Instrument.create!(name: "squarewave")
+
 instruments = Instrument.all
 
 12.times do |i|
@@ -34,9 +36,9 @@ instruments.each do |instrument|
   end
 end
 
-reverb = Reverb.create!(name: "AirportTerminal", upload: File.open("#{Rails.root}/public/reverbs/AirportTerminal.wav"), upload_content_type: "audio/wav")
+airport = Reverb.create!(name: "AirportTerminal", upload: File.open("#{Rails.root}/public/reverbs/AirportTerminal.wav"), upload_content_type: "audio/wav")
 
-scale = Scale.create!(name: "major", degrees: [0,2,4,5,7,9,11])
+major = Scale.create!(name: "major", degrees: [0,2,4,5,7,9,11])
 Scale.create!(name: "minor", degrees: [0,2,3,5,7,8,10])
 Scale.create!(name: "penta", degrees: [0,2,5,7,9])
 
@@ -44,23 +46,26 @@ TRIAD_0 = [0,2,4]
 TRIAD_1 = [0,2,5]
 TRIAD_2 = [0,3,5]
 
-chord = Chord.create!(name: 'triad', intervals: [TRIAD_0[0], TRIAD_0[1], TRIAD_0[2]])
+triad = Chord.create!(name: 'triad', intervals: [TRIAD_0[0], TRIAD_0[1], TRIAD_0[2]])
 Chord.create!(name: 'triad_6', intervals: [TRIAD_1[0], TRIAD_1[1], TRIAD_1[2]])
 Chord.create!(name: 'triad_64', intervals: [TRIAD_2[0], TRIAD_2[1], TRIAD_2[2]])
 
 user = User.create!(email: "user@example.com", password: "password")
 UserInstrument.create!(user: user, instrument: piano)
-UserScale.create!(user: user, scale: scale)
-UserReverb.create!(user: user, reverb: reverb)
-UserChord.create!(user: user, chord: chord)
+UserScale.create!(user: user, scale: major)
+UserReverb.create!(user: user, reverb: airport)
+UserChord.create!(user: user, chord: triad)
 
-Key.all.each do |key|
-  Scale.all.each do |s|
+keys = Key.all
+scales = Scale.all
+
+keys.each do |key|
+  scales.each do |scale|
     12.times do |i|
       if scale.degrees.map{ |n| n + key.transposition % 12 }.include?(i)
-        ScaleNote.create!(scale: s, note: Note.find_by(name: NOTES[i], instrument: piano), key: key)
-        ScaleNote.create!(scale: s, note: Note.find_by(name: NOTES[i], instrument: harp), key: key)
-        ScaleNote.create!(scale: s, note: Note.find_by(name: NOTES[i], instrument: marimba), key: key)
+        instruments.each do |inst|
+          ScaleNote.create!(scale: scale, note: Note.find_by(name: NOTES[i], instrument: inst), key: key)
+        end
       end
     end
   end

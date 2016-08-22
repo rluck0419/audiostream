8dd37ce188da29e372ce2074774c987fd7cc4c3c
Robert Luckfield <rluck0419@gmail.com>
2016-08-22 15:22:13 -0500

    console logs user-out

--- app/jobs/user_disappear_job.rb
+++ app/jobs/user_disappear_job.rb
@@ -2,6 +2,6 @@ class UserDisappearJob < ApplicationJob
   queue_as :default
 
   def perform(user)
-    ActionCable.server.broadcast("appearance", message: "#{user.email} has logged out")
+    ActionCable.server.broadcast("appearance", message: "#{user.email} has left")
   end
 end
